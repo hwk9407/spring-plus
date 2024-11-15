6a3d7b89856436994757f3d5d8e5c2ac8ac0df1d
hwk9407 <hwk__@naver.com>
2024-11-15 16:35:44 +0900

    Level. 2-9 Spring Security 도입
    - Spring Security 활성화에 따른 AOP 에 관리자 권한 확인 로직 추가

--- src/main/java/org/example/expert/aop/AdminAccessLoggingAspect.java
+++ src/main/java/org/example/expert/aop/AdminAccessLoggingAspect.java
@@ -4,9 +4,11 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
@@ -26,12 +28,21 @@ public class AdminAccessLoggingAspect {
      * 적용 범위: `UserAdminController` 클래스의 `changeUserRole()`
      */
     @Before("execution(* org.example.expert.domain.user.controller.UserAdminController.changeUserRole(..))")
-    public void logAfterChangeUserRole(JoinPoint joinPoint) {
+    public void logBeforeChangeUserRole(JoinPoint joinPoint) {
+        if (!isAdminUser()) throw new AccessDeniedException("관리자 권한이 없습니다.");
+
         String userId = String.valueOf(request.getAttribute("userId"));
         String requestUrl = request.getRequestURI();
         LocalDateTime requestTime = LocalDateTime.now();
 
         log.info("Admin Access Log - User ID: {}, Request Time: {}, Request URL: {}, Method: {}",
                 userId, requestTime, requestUrl, joinPoint.getSignature().getName());
+    }
+
+    private boolean isAdminUser() {
+        // 현재 인증된 사용자가 ADMIN 권한을 가지고 있는지 확인
+        UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return userDetails.getAuthorities().stream()
+                .anyMatch(authority -> authority.getAuthority().equals("ADMIN"));
     }
 }
