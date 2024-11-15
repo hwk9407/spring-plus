cea56d2d62ad4063eaa16cc7b322d8cf22ab701a
hwk9407 <hwk__@naver.com>
2024-11-15 16:27:37 +0900

    Level. 2-9 Spring Security 도입
    - Secured 제거 및 주석 정리

--- src/main/java/org/example/expert/domain/user/controller/UserAdminController.java
+++ src/main/java/org/example/expert/domain/user/controller/UserAdminController.java
@@ -3,7 +3,6 @@ package org.example.expert.domain.user.controller;
 import lombok.RequiredArgsConstructor;
 import org.example.expert.domain.user.dto.request.UserRoleChangeRequest;
 import org.example.expert.domain.user.service.UserAdminService;
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,7 +15,6 @@ public class UserAdminController {
     private final UserAdminService userAdminService;
 
     @PatchMapping("/admin/users/{userId}")
-    @Secured("ADMIN")
     public void changeUserRole(@PathVariable long userId, @RequestBody UserRoleChangeRequest userRoleChangeRequest) {
         userAdminService.changeUserRole(userId, userRoleChangeRequest);
     }
--- src/main/java/org/example/expert/security/config/WebSecurityConfig.java
+++ src/main/java/org/example/expert/security/config/WebSecurityConfig.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -19,7 +18,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(securedEnabled = true) // @Secured 어노테이션 활성화
 @RequiredArgsConstructor
 public class WebSecurityConfig {
 
@@ -28,7 +26,7 @@ public class WebSecurityConfig {
     private final AuthenticationConfiguration authenticationConfiguration;
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager(); // AuthenticationManager 수동 Bean 추가
+        return authenticationConfiguration.getAuthenticationManager();
     }
     @Bean
     public JwtAuthenticationFilter jwtAuthenticationFilter() throws Exception {
@@ -55,21 +53,21 @@ public class WebSecurityConfig {
         http.authorizeHttpRequests((authorizeHttpRequests) ->
                 authorizeHttpRequests
                         .requestMatchers("/auth/**").permitAll() // '/auth/'로 시작하는 요청 모두 접근 허가
-                        .anyRequest().authenticated() // 그 외 모든 요청 인증처리
+                        .anyRequest().authenticated()
         );
 
-        // 필터 관리 jwtAuthorizationFilter -> jwtAuthenticationFilter
+        // 필터 관리
         http.addFilterBefore(jwtAuthorizationFilter(), JwtAuthenticationFilter.class);
         http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 
-        /*// 예외 발생 시 처리
+        // 예외 발생 시 처리
         http.exceptionHandling(exceptionHandling ->
                 exceptionHandling.accessDeniedHandler((request, response, accessDeniedException) -> {
                     response.setStatus(HttpServletResponse.SC_FORBIDDEN); // 403
                     response.setContentType("application/json");
                     response.getWriter().write("{\"error\":\"Access Denied\"}");
                 })
-        );*/
+        );
         return http.build();
 
     }
